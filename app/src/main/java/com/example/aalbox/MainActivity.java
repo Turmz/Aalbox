758ec82cd50509e93015b9e59d085dfd29f16c3a
Tummas Jóhan Sigvardsen <tsigva16@student.aau.dk>
2020-04-13 01:47:13 +0200

    Added top and bottom bar. The hovering "+" not included.
    Maybe only an initial commit, don't know if this is the best solution.

--- app/src/main/java/com/example/aalbox/MainActivity.java
+++ app/src/main/java/com/example/aalbox/MainActivity.java
@@ -9,17 +9,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.List;
+/*
+import com.example.aalbox.fragments.FeedFragment;
+import com.example.aalbox.fragments.MapFragment;
+*/
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigation;
     private PostViewModel postViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(com.example.aalbox.fragments.FeedFragment.newInstance("", ""));
 
         RecyclerView recyclerView = findViewById(R.id.recView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -55,6 +70,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_feed:
+                            openFragment(com.example.aalbox.fragments.FeedFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_map:
+                            openFragment(com.example.aalbox.fragments.MapFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
 
     public void changeToAddPostView(View view){
         Intent intent = new Intent(this, AddPostActivity.class);
